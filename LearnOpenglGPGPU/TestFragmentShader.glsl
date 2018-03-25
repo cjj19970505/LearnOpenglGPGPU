b8e7c8b61ef0fe3d5c376cc4732e4bf42c053db0
陈俊嘉 <cjj19970505@live.cn>
2018-03-25 15:33:35 +0800

    FInally working

--- LearnOpenglGPGPU/TestFragmentShader.glsl
+++ LearnOpenglGPGPU/TestFragmentShader.glsl
@@ -1,7 +1,6 @@
 #version 430 core
 out vec4 FragColor;
-layout(rgba32f, binding = 0) uniform imageBuffer testImageBuffer;
-
+layout (binding = 0) uniform sampler2D output_image;
 in VS_OUT
 {
 	vec4 vertexColor;
@@ -16,12 +15,6 @@ struct App
 uniform App app;
 void main()
 {
-	vec4 pos = imageLoad(testImageBuffer, 0);
 	FragColor = fs_in.vertexColor;
-	FragColor = pos;
-	int PARTICLE_COUNT = 100;
-	for(int i = 0; i<PARTICLE_COUNT; i++){
-		imageStore(testImageBuffer, PARTICLE_COUNT, vec4(2,2,2,2));
-	}
-	
+	FragColor = texture(output_image, vec2(1.0,1.0));
 }
